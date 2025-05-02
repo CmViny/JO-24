66ec0ec77dc442400b5d82dcd46f6cf51792754b
VinyCm <viny.come@epitech.eu>
2025-05-02 20:15:00 +0200

    Login and Logout

--- JOStudi/accounts/views.py
+++ JOStudi/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Vous avez été connecté avec succès.")
+            return redirect('home')
+        else:
+            messages.error(request, "Identifiants invalides, veuillez réessayer.")
+            return redirect('login')
+            
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Vous avez été déconnecté avec succès.")
+    return redirect('home')
