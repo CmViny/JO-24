a0181e5ac78cfa55af68393b871fe835b74dcece
VinyCm <viny.come@epitech.eu>
2025-05-20 23:06:50 +0200

    Fix settings

--- JOStudi/JOStudi/settings.py
+++ JOStudi/JOStudi/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
         'USER' : os.environ.get('DB_USER'),
-        'PASSWORD' : os.environ('DB_PASSWORD'),
+        'PASSWORD' : os.environ.get('DB_PASSWORD'),
         'HOST' : os.environ.get('DB_HOST'),
         'PORT' : os.environ.get('DB_PORT'),    
     }
