de6444d63530a5b3c652494cf2deb89a370fe171
VinyCm <viny.come@epitech.eu>
2025-05-21 00:20:24 +0200

    Fix env variables for railways

--- JOStudi/JOStudi/settings.py
+++ JOStudi/JOStudi/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
