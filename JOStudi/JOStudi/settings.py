d0fbf6eef8499ced2f271155c4479dc54da3e700
VinyCm <viny.come@epitech.eu>
2025-05-21 02:13:52 +0200

    Update settings cloudinary

--- JOStudi/JOStudi/settings.py
+++ JOStudi/JOStudi/settings.py
@@ -117,6 +117,12 @@ CSRF_COOKIE_HTTPONLY = True
 # Config Cloudinary
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
 cloudinary.config(
     cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key = os.getenv('CLOUDINARY_API_KEY'),
