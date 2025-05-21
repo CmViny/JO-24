3cc79682e01013dbc92288b1c40e73bbbe6e9dbb
VinyCm <comeviny@gmail.com>
2025-05-21 00:35:42 +0000

    MR Update Offers Models for Cloudinary

--- JOStudi/JOStudi/settings.py
+++ JOStudi/JOStudi/settings.py
@@ -131,6 +131,13 @@ cloudinary.config(
     secure = True
 )
 
+cloudinary.config(
+    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key = os.getenv('CLOUDINARY_API_KEY'),
+    api_secret = os.getenv('CLOUDINARY_API_SECRET'),
+    secure = True
+)
+
 # Configuration de la base de données
 DATABASES = {
     'default': {
